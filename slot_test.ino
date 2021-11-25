938b2ce63f15c63a70376cfb06511f0496eccdad
taiga53 <78756511+taiga53@users.noreply.github.com>
2021-11-25 10:16:42 -0500

    Add files via upload
    11-25 new

--- slot_test.ino
+++ slot_test.ino
@@ -1,20 +1,23 @@
-int top[7] = {2,3,4,5,6,7,8};
-int bot[7] = {9,10,11,12,13,A3,A2};
+int bot[7] = {2,3,4,5,6,7,8};
+int top[7] = {9,10,11,12,13,A3,A2};
 
 void setup() {
-  
+
   for (int i = 0; i <= 7; i++){
-    pinMode(top[i],OUTPUT);
+    pinMode(bot[i],OUTPUT);
     pinMode(bot[i],OUTPUT);
   }
 }
 
 void one() {
+  
   for(int i = 0; i <= 7; i++){
     if (i == 1 || i == 2){
+      digitalWrite(bot[i],HIGH);
       digitalWrite(top[i],HIGH);
     }
     else {
+      digitalWrite(bot[i],LOW);
       digitalWrite(top[i],LOW);
     }
   }
@@ -24,9 +27,11 @@ void two() {
   
   for(int i = 0; i <= 7; i++){
     if (i == 2 || i == 5){
+      digitalWrite(bot[i],LOW);
       digitalWrite(top[i],LOW);
     }
     else {
+      digitalWrite(bot[i],HIGH);
       digitalWrite(top[i],HIGH);
     }
   }
@@ -36,9 +41,11 @@ void three() {
   
   for(int i = 0; i <= 7; i++){
     if (i == 4 || i == 5){
+      digitalWrite(bot[i],LOW);
       digitalWrite(top[i],LOW);
     }
     else {
+      digitalWrite(bot[i],HIGH);
       digitalWrite(top[i],HIGH);
     }
   }
@@ -48,9 +55,11 @@ void four() {
   
   for(int i = 0; i <= 7; i++){
     if (i == 0 || i == 3 || i == 4){
+      digitalWrite(bot[i],LOW);
       digitalWrite(top[i],LOW);
     }
     else {
+      digitalWrite(bot[i],HIGH);
       digitalWrite(top[i],HIGH);
     }
   }
@@ -60,9 +69,11 @@ void five() {
   
   for(int i = 0; i <= 7; i++){
     if (i == 1 || i == 4){
+      digitalWrite(bot[i],LOW);
       digitalWrite(top[i],LOW);
     }
     else {
+      digitalWrite(bot[i],HIGH);
       digitalWrite(top[i],HIGH);
     }
   }
@@ -72,9 +83,11 @@ void six() {
   
   for(int i = 0; i <= 7; i++){
     if (i == 1){
+      digitalWrite(bot[i],LOW);
       digitalWrite(top[i],LOW);
     }
     else {
+      digitalWrite(bot[i],HIGH);
       digitalWrite(top[i],HIGH);
     }
   }
@@ -84,9 +97,11 @@ void seven() {
   
   for(int i = 0; i <= 7; i++){
     if (i == 0 || i == 1 || i == 2){
+      digitalWrite(bot[i],HIGH);
       digitalWrite(top[i],HIGH);
     }
     else {
+      digitalWrite(bot[i],LOW);
       digitalWrite(top[i],LOW);
     }
   }
@@ -95,6 +110,7 @@ void seven() {
 void eight() {
   
   for(int i = 0; i <= 7; i++){
+    digitalWrite(bot[i],HIGH);
     digitalWrite(top[i],HIGH);
   }
 }
@@ -103,9 +119,11 @@ void nine() {
   
   for(int i = 0; i <= 7; i++){
     if (i == 3 || i == 4){
+      digitalWrite(bot[i],LOW);
       digitalWrite(top[i],LOW);
     }
     else {
+      digitalWrite(bot[i],HIGH);
       digitalWrite(top[i],HIGH);
     }
   }
@@ -115,17 +133,25 @@ void zero() {
   
   for(int i = 0; i <= 7; i++){
     if (i == 6){
+      digitalWrite(bot[i],LOW);
       digitalWrite(top[i],LOW);
     }
     else {
+      digitalWrite(bot[i],HIGH);
       digitalWrite(top[i],HIGH);
     }
   }
 }
 
-int numbers[10] = {one,two,three,four,five,six,seven,eight,nine,zero};
+void cycle() {
   
-void (numbers[10])();
+int (*nums[10])() = {one,two,three,four,five,six,seven,eight,nine,zero};
+  
+  for (int i = 0; i <= 9; i ++) {
+    nums[i]();
+    delay(750);
+  }
+}
 
 void loop() {
   cycle();
