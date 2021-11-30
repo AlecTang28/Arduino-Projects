5da56b5672c9f4c8043c5365246d06c54a6d3268
taiga53 <1tangale@hdsb.ca>
2021-11-30 12:46:19 -0500

    Update Slots.ino

--- Slots/Slots.ino
+++ Slots/Slots.ino
@@ -1,10 +1,11 @@
-
 int bot[7] = {2,3,4,5,6,7,8};
 int top[7] = {9,10,11,12,13,A3,A2};
 long randNum;
 
 void setup() {
 
+  Serial.begin(9600);
+  pinMode(A0,INPUT);
   for (int i = 0; i <= 6; i++){
     pinMode(bot[i],OUTPUT);
     pinMode(top[i],OUTPUT);
@@ -12,15 +13,13 @@ void setup() {
 }
 
 void one() {
-  
+
   for(int i = 0; i <= 6; i++){
     if (i == 1 || i == 2){
       digitalWrite(bot[i],HIGH);
-      digitalWrite(top[i],HIGH);
     }
     else {
       digitalWrite(bot[i],LOW);
-      digitalWrite(top[i],LOW);
     }
   }
 }
@@ -30,11 +29,9 @@ void two() {
   for(int i = 0; i <= 6; i++){
     if (i == 2 || i == 5){
       digitalWrite(bot[i],LOW);
-      digitalWrite(top[i],LOW);
     }
     else {
       digitalWrite(bot[i],HIGH);
-      digitalWrite(top[i],HIGH);
     }
   }
 }
@@ -44,11 +41,9 @@ void three() {
   for(int i = 0; i <= 6; i++){
     if (i == 4 || i == 5){
       digitalWrite(bot[i],LOW);
-      digitalWrite(top[i],LOW);
     }
     else {
       digitalWrite(bot[i],HIGH);
-      digitalWrite(top[i],HIGH);
     }
   }
 }
@@ -58,11 +53,9 @@ void four() {
   for(int i = 0; i <= 6; i++){
     if (i == 0 || i == 3 || i == 4){
       digitalWrite(bot[i],LOW);
-      digitalWrite(top[i],LOW);
     }
     else {
       digitalWrite(bot[i],HIGH);
-      digitalWrite(top[i],HIGH);
     }
   }
 }
@@ -72,11 +65,9 @@ void five() {
   for(int i = 0; i <= 6; i++){
     if (i == 1 || i == 4){
       digitalWrite(bot[i],LOW);
-      digitalWrite(top[i],LOW);
     }
     else {
       digitalWrite(bot[i],HIGH);
-      digitalWrite(top[i],HIGH);
     }
   }
 }
@@ -86,11 +77,9 @@ void six() {
   for(int i = 0; i <= 6; i++){
     if (i == 1){
       digitalWrite(bot[i],LOW);
-      digitalWrite(top[i],LOW);
     }
     else {
       digitalWrite(bot[i],HIGH);
-      digitalWrite(top[i],HIGH);
     }
   }
 }
@@ -100,11 +89,9 @@ void seven() {
   for(int i = 0; i <= 6; i++){
     if (i == 0 || i == 1 || i == 2){
       digitalWrite(bot[i],HIGH);
-      digitalWrite(top[i],HIGH);
     }
     else {
       digitalWrite(bot[i],LOW);
-      digitalWrite(top[i],LOW);
     }
   }
 }
@@ -113,7 +100,6 @@ void eight() {
   
   for(int i = 0; i <= 6; i++){
     digitalWrite(bot[i],HIGH);
-    digitalWrite(top[i],HIGH);
   }
 }
 
@@ -122,11 +108,9 @@ void nine() {
   for(int i = 0; i <= 6; i++){
     if (i == 3 || i == 4){
       digitalWrite(bot[i],LOW);
-      digitalWrite(top[i],LOW);
     }
     else {
       digitalWrite(bot[i],HIGH);
-      digitalWrite(top[i],HIGH);
     }
   }
 }
@@ -136,23 +120,152 @@ void zero() {
   for(int i = 0; i <= 6; i++){
     if (i == 6){
       digitalWrite(bot[i],LOW);
-      digitalWrite(top[i],LOW);
     }
     else {
       digitalWrite(bot[i],HIGH);
+    }
+  }
+}
+
+void one2() {
+  
+  for(int i = 0; i <= 6; i++){
+    if (i == 1 || i == 2){
+      digitalWrite(top[i],HIGH);
+    }
+    else {
+      digitalWrite(top[i],LOW);
+    }
+  }
+}
+
+void two2() {
+  
+  for(int i = 0; i <= 6; i++){
+    if (i == 2 || i == 5){
+      digitalWrite(top[i],LOW);
+    }
+    else {
+      digitalWrite(top[i],HIGH);
+    }
+  }
+}
+
+void three2() {
+  
+  for(int i = 0; i <= 6; i++){
+    if (i == 4 || i == 5){
+      digitalWrite(top[i],LOW);
+    }
+    else {
+      digitalWrite(top[i],HIGH);
+    }
+  }
+}
+
+void four2() { 
+
+  for(int i = 0; i <= 6; i++){
+    if (i == 0 || i == 3 || i == 4){
+      digitalWrite(top[i],LOW);
+    }
+    else {
+      digitalWrite(top[i],HIGH);
+    }
+  }
+}
+
+void five2() {
+  
+  for(int i = 0; i <= 6; i++){
+    if (i == 1 || i == 4){
+      digitalWrite(top[i],LOW);
+    }
+    else {
+      digitalWrite(top[i],HIGH);
+    }
+  }
+}
+
+void six2() {
+  
+  for(int i = 0; i <= 6; i++){
+    if (i == 1){
+      digitalWrite(top[i],LOW);
+    }
+    else {
+      digitalWrite(top[i],HIGH);
+    }
+  }
+}
+
+void seven2() {
+
+  for(int i = 0; i <= 6; i++){
+    if (i == 0 || i == 1 || i == 2){
+      digitalWrite(top[i],HIGH);
+    }
+    else {
+      digitalWrite(top[i],LOW);
+    }
+  }
+}
+
+void eight2() {
+  
+  for(int i = 0; i <= 6; i++){
+    digitalWrite(top[i],HIGH);
+  }
+}
+
+void nine2() {
+  
+  for(int i = 0; i <= 6; i++){
+    if (i == 3 || i == 4){
+      digitalWrite(top[i],LOW);
+    }
+    else {
+      digitalWrite(top[i],HIGH);
+    }
+  }
+}
+
+void zero2() {
+  
+  for(int i = 0; i <= 6; i++){
+    if (i == 6){
+      digitalWrite(top[i],LOW);
+    }
+    else {
       digitalWrite(top[i],HIGH);
     }
   }
 }
 
 int (*nums[10])() = {one,two,three,four,five,six,seven,eight,nine,zero};
+int (*nums2[10])() = {one2,two2,three2,four2,five2,six2,seven2,eight2,nine2,zero2};
 
 void cycle() {
   randNum = random(0,10);
   nums[randNum]();
-  delay(100);
+  randNum = random(0,10);
+  nums2[randNum]();
+  delay(75);
 }
 
 void loop() {
+
+  int topList;
+  int botList;
+  int pressed;
+  
   cycle();
+  //pushbutton default state is HIGH)
+  if(digitalRead(A0) == LOW){
+    pressed = 1;
+  }
+  else{
+    pressed = 0;
+  }
+  Serial.println(pressed);
 }
